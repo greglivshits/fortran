8912e7f0d56330f2ffa5a0c363514ef5535bb5c5
greglivshits <56725826+greglivshits@users.noreply.github.com>
2019-10-22 00:32:05 +0300

    Update FirstHomework.f90

--- FirstHomework.f90
+++ FirstHomework.f90
@@ -19,14 +19,14 @@
     Character(255) Comment
     Integer(4), parameter::MaxAt=100
     Integer(4) NA(MaxAt)
-    Real(8) C(3,MaxAt), S(MaxAt), cm(3), x(MaxAt), TI(3,3)
+    Real(8) C(3,MaxAt), S(MaxAt), cm(3), x(MaxAt), TI(3,3), cp(3,MaxAt)
     ! Variables
 
     ! Body of Console1
     ! Openning and reading
     Open (5, File  ='h2o.xyz')
         Read (5,*)NumAt
-        Read (5,*)Comment
+        Read (5,'(a255)')Comment
         Totmass=0
         Do i=1,NumAt
             Read(5,*) NA(i), C(1:3, i)
@@ -36,30 +36,35 @@
     Close (5)
     ! MassCenter coordinates
         Do i=1,3
-            x(i)=0
+            x(i)=0.d0
             Do j=1,NumAt
                 x(i)=x(i)+S(j)*C(i,j)
             Enddo
             cm(i) = x(i)/Totmass
         Enddo
-    ! Moment of inertia
-        TI(1:3, 1:3)=0
+    ! Coordinates about MassCenter
         Do i=1,NumAt
-            TI(1,1)=TI(1,1)+S(i)*((C(2,i)-cm(2))**2+(C(3,i)-cm(3))**2)
-            TI(1,2)=TI(1,2)-S(i)*(C(1,i)-cm(1))*(C(2,i)-cm(2))
-            TI(1,3)=TI(1,3)-S(i)*(C(1,i)-cm(1))*(C(3,i)-cm(3))
-            TI(2,1)=TI(2,1)-S(i)*(C(1,i)-cm(1))*(C(2,i)-cm(2))
-            TI(2,2)=TI(2,2)+S(i)*((C(1,i)-cm(1))**2+(C(3,i)-cm(3))**2)
-            TI(2,3)=TI(2,3)-S(i)*(C(2,i)-cm(2))*(C(3,i)-cm(3))
-            TI(3,1)=TI(3,1)-S(i)*(C(3,i)-cm(3))*(C(1,i)-cm(1))
-            TI(3,2)=TI(3,2)-S(i)*(C(3,i)-cm(3))*(C(2,i)-cm(2))
-            TI(3,3)=TI(3,3)+S(i)*((C(1,i)-cm(1))**2+(C(2,i)-cm(2))**2)
+            cp(1:3,i) = C(1:3,i)-cm(1:3)
+        Enddo
+    ! Moment of inertia
+        TI(1:3, 1:3)=0.d0
+        Do i=1,NumAt
+            TI(1,1)=TI(1,1)+S(i)*(cp(2,i)**2+cp(3,i)**2)
+            TI(1,2)=TI(1,2)-S(i)*cp(1,i)*cp(2,i)
+            TI(1,3)=TI(1,3)-S(i)*cp(1,i)*cp(3,i)
+            TI(2,1)=TI(2,1)-S(i)*cp(1,i)*cp(2,i)
+            TI(2,2)=TI(2,2)+S(i)*(cp(1,i)**2+cp(3,i)**2)
+            TI(2,3)=TI(2,3)-S(i)*cp(2,i)*cp(3,i)
+            TI(3,1)=TI(3,1)-S(i)*cp(3,i)*cp(1,i)
+            TI(3,2)=TI(3,2)-S(i)*cp(3,i)*cp(2,i)
+            TI(3,3)=TI(3,3)+S(i)*(cp(1,i)**2+cp(2,i)**2)
         Enddo
     ! Writing
     Open (6, File ='h2o.out')
         Write(6,'(/32x,''*** Program ReadXYZ ***''/)')
         Write (6,*)NumAt
         Write (6,*)Comment
+        Write(6,'("Atomic Coordinates")')
         Do i=1,NumAt
             Write(6,'(4f15.5)') S(i), c(1, i), c(2, i), c(3, i)
         Enddo
