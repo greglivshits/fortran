05fa430dbc37405ecd774eadd2bbe69eb13e99c5
greglivshits <56725826+greglivshits@users.noreply.github.com>
2019-10-30 09:55:46 +0300

    Update Ortogonality.f90

--- Ortogonality.f90
+++ Ortogonality.f90
@@ -1,12 +1,12 @@
-SubRoutine Ortogonality(iu,a)
+SubRoutine Ortogonality(iu,a,eps)
  Real(8) a(3,3)
- Real(8) b,c,d,x,y,z
-If (a(1,1)*a(2,1)+a(1,2)*a(2,2)+a(1,3)*a(2,3) == 0 .and. a(2,1)*a(3,1)+a(2,2)*a(3,2)+a(2,3)*a(3,3) == 0 .and. a(1,1)*a(3,1)+a(1,2)*a(3,2)+a(1,3)*a(3,3) == 0) then
+ Real(8) eps,b,c,d,x,y,z
+If (DABS(a(1,1)*a(2,1)+a(1,2)*a(2,2)+a(1,3)*a(2,3) - 0.d0)<eps .and. DABS(a(2,1)*a(3,1)+a(2,2)*a(3,2)+a(2,3)*a(3,3) - 0.d0)<eps .and. DABS(a(1,1)*a(3,1)+a(1,2)*a(3,2)+a(1,3)*a(3,3) - 0.d0)<eps) then
         write(iu,'("ortogonal")')
     Else 
         write(iu,'("not ortogonal")')
     EndIf
- If (a(1,1)*a(1,1)+a(1,2)*a(1,2)+a(1,3)*a(1,3) == 1 .and. a(2,1)*a(2,1)+a(2,2)*a(2,2)+a(2,3)*a(2,3) == 1 .and. a(3,1)*a(3,1)+a(3,2)*a(3,2)+a(3,3)*a(3,3) == 1) then
+ If (DABS(a(1,1)*a(1,1)+a(1,2)*a(1,2)+a(1,3)*a(1,3) - 1.d0)<eps .and. DABS(a(2,1)*a(2,1)+a(2,2)*a(2,2)+a(2,3)*a(2,3) - 1.d0)<eps .and. DABS(a(3,1)*a(3,1)+a(3,2)*a(3,2)+a(3,3)*a(3,3) - 1.d0)<eps) then
         write(iu,'("normalized")') 
     Else 
         write(iu,'("not normalized")')
